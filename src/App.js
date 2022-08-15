87ad21ccfc6d9619beb5b4fb28a2507de182c5b5
Rahul <rahulmondalbnk258@gmail.com>
2022-08-15 23:31:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  return(
+      <div id = "app">
+          <Profile/>
+          <Home/>
+          <Register />
+          <Login />
+
+      </div>
+  )
+
+
+}
+
+export default App;
